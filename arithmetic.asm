18a8c9a8a014b19edf194309ff17cef16a642304
Vincent <vincentzhu@ucsb.edu>
2022-01-29 23:07:08 -0800

    Done

--- arithmetic.asm
+++ arithmetic.asm
@@ -2,11 +2,41 @@
 # A simple calculator program in MIPS Assembly for CS64
 #
 
+.data
+	prompt: .asciiz "Enter a integer: "
+
 .text
 main:
-
 	# TODO: Write your code here
+	# Get user input 1 (integer)
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# Get user input 2 (integer)
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	li $v0, 5
+	syscall
+	move $t1, $v0
+
+	# Calculate 64*(x - y) + 32
+	sub $t2, $t0, $t1
+	sll $t2, $t2, 6
+	addi $t2, 32
+
+	# Print result 
+	li $v0, 1
+	move $a0, $t2
+	syscall
+	j exit
 
 exit:
 	# Exit SPIM: Write your code here!
+	li $v0 10
+	syscall
 
--- hello.asm
+++ hello.asm
@@ -1,16 +1,52 @@
 # hello.asm
 # A "Hello World" program in MIPS Assembly for CS64
 #
-#  Data Area - allocate and initialize variables
+# Data Area - allocate and initialize variables
 .data
 	# TODO: Write your string definitions here
+	prompt: .asciiz "Enter a integer: "
+	one: .asciiz "integer is odd\n"
+	notone: .asciiz "integer is even\n"
 
-#Text Area (i.e. instructions)
+# Text Area (i.e. instructions)
 .text
 main:
-
 	# TODO: Write your code here
+	# Get user input (integer)
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# Check if input is even or odd
+	li $t1, 1
+	and $t2, $t0, $t1
+	beq $t2, $zero, even
+	bne $t2, $zero, odd
+
+# If even
+even:
+	li $t3, 6
+	mult $t0, $t3
+	mflo $t4
+	li $v0 1
+	move $a0 $t4
+	syscall
+	j exit
+
+# If odd
+odd:
+	li $t3, 9
+	mult $t0, $t3
+	mflo $t4
+	li $v0 1
+	move $a0 $t4
+	syscall
+	j exit
 
 exit:
 	# Exit SPIM: Write your code here!
-
+	li $v0, 10
+	syscall
